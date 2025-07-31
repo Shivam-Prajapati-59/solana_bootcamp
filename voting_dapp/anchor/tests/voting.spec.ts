b9c2c52ce85b31edc30e9a710b407644eea3c087
Shivam Prajapati <sp488830@gmail.com>
2025-07-31 13:46:36 +0530

    Completed the build and deployemnt of contrcat on local net

---
+++ voting_dapp/anchor/tests/voting.spec.ts
@@ -0,0 +1,10 @@
+import * as anchor from "@coral-xyz/anchor";
+import { Program } from "@coral-xyz/anchor";
+
+describe("voting", async () => {
+
+    it('Initialize Poll', async () => {
+        
+    })
+
+})
