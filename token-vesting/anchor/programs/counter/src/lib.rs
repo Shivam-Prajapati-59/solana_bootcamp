cc94edb746ff2f52647dd0838b0c9560a5ab5ab5
Shivam Prajapati <sp488830@gmail.com>
2025-08-27 22:51:55 +0530

    Implement create_employee_account function and associated account struct

--- token-vesting/anchor/programs/counter/src/lib.rs
+++ token-vesting/anchor/programs/counter/src/lib.rs
@@ -21,6 +21,20 @@ pub mod vesting {
 
         Ok(())
     }
+
+    pub fn create_employee_account(ctx: Context<CreateEmployeeAccount>, start_time:i64, end_time:i64, total_amount: u64,
+    cliff_time: i64) ->Result<()> {
+        *ctx.account.employee_account = EmployeeAccount {
+            beneficiary : ctx.accounts.beneficiary.key(),
+            start_time,
+            end_time,
+            total_amount,
+            total_withdrawn: 0,
+            cliff_time,
+            vesting_account: ctx.accounts.vesting_account.key()<
+            bump: ctx.bumps.employee_account,
+        }
+    }
 }
 
 #[derive(Accounts)]
@@ -50,10 +64,34 @@ pub struct CreateVestingAccount<'info> {
     )]
 
     pub treasury_token_account: InterfaceAccount<'info, TokenAccount>,
-
-    pub system_program: Interface<'info, TokenInterface>
+    pub system_program: Program<'info, System>,
+    pub token_program: Interface<'info, TokenInterface>
     
 }
+
+#[derive(Accounts)]
+pub  struct CreateEmployeeAccount<'info> {
+    #[account(mut)]
+    pub owner: Signer<'info>,
+    pub beneficiary: SystemAccount<'info>,
+    #[account(
+        has_one = owner,
+    )]
+    pub vesting_account: Account<'info, VestingAccount>,
+    #[account(
+        init,
+        space = 8 + EmployeeAccount::INIT_SPACE,
+        payer = owner,
+        seeds = [b"employee_vesting", beneficiary.key().as_ref()],
+        bump,
+    )]
+
+    pub employee_account : Account<'info, EmployeeAccount>,
+    pub system_program: Program<'info, System>,
+
+}
+
+
 #[account]
 #[derive(InitSpace)]
 pub struct VestingAccount{
@@ -65,4 +103,17 @@ pub struct VestingAccount{
     pub treasury_bump: u8,
     pub bump: u8
 
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct EmployeeAccount {
+    pub beneficiary: PubKey,
+    pub start_time: i64,
+    pub end_time: i64,
+    pub cliff_time: i64,
+    pub vesting_account: PubKey,
+    pub total_amount: u64,
+    pub total_withdrawn: u64,
+    pub bump: u8,
 }
